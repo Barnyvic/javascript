5e8ecbc74b063d96cf0500039f4f8838f24f86ed
Barny victor <victorbarny4@gmail.com>
2022-06-24 09:25:33 +0100

    finished

---
+++ index.js
@@ -0,0 +1,15 @@
+const Name = document.getElementById("name");
+const Height = document.getElementById("height");
+const Country = document.getElementById("country");
+
+Name.innerText = "Name: " + "Barny Victor";
+Height.innerText = "Height: " + "5.75m";
+Country.innerText = "Country: " + "Nigeria";
+
+let myName = "Barny Victor";
+let myHeight = "5.75m";
+let myCountry = "Nigeria";
+
+alert(`My Name is: ${myName}`);
+alert(`My Height is: ${myHeight}`);
+alert(`My Country Name is: ${myCountry}`);
